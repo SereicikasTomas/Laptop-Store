69645a29c1d374e80e10559a90e73f6c2ebfda71
jacando.tos <sereicikas.t@gmail.com>
2020-03-17 17:21:20 +0100

    add routes

--- index.js
+++ index.js
@@ -8,15 +8,17 @@ const laptopData = JSON.parse(json);
 
 const server = http.createServer((req, res) => {
   const pathName = url.parse(req.url, true).pathname;
-  if(pathName === "/products" || pathName === "/"){
+  const laptopId = url.parse(req.url, true).query.id;
+
+  if (pathName === "/products" || pathName === "/") {
     res.writeHead(200, { "Content-type": "text/html" });
-    res.end("This is the Products page."); 
-  }else if(pathName === "/laptop"){
+    res.end("This is the Products page.");
+  } else if (pathName === "/laptop" && laptopId < laptopData.length) {
     res.writeHead(200, { "Content-type": "text/html" });
-    res.end("This is the Laptop page."); 
-  }else{
+    res.end(`This is the Laptop page for laptop ${laptopId}.`);
+  } else {
     res.writeHead(404, { "Content-type": "text/html" });
-    res.end("This is the 404 page."); 
+    res.end("This is the 404 page.");
   }
 });
 
