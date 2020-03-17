f2445a9b771f7e77994a8385db8b773fc55bb672
jacando.tos <sereicikas.t@gmail.com>
2020-03-17 17:09:07 +0100

    add routes

--- index.js
+++ index.js
@@ -6,8 +6,17 @@ const json = fs.readFileSync(`${__dirname}/data/data.json`, "utf-8");
 const laptopData = JSON.parse(json);
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-type": "text/html" });
-  res.end("This is the response!");
+  const pathName = url.parse(req.url, true).pathname;
+  if(pathName === "/products" || pathName === "/"){
+    res.writeHead(200, { "Content-type": "text/html" });
+    res.end("This is the Products page."); 
+  }else if(pathName === "/laptop"){
+    res.writeHead(200, { "Content-type": "text/html" });
+    res.end("This is the Laptop page."); 
+  }else{
+    res.writeHead(404, { "Content-type": "text/html" });
+    res.end("This is the 404 page."); 
+  }
 });
 
 server.listen(1337, "127.0.0.1", () => {
