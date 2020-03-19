64e7077742573eea6892e1eedc0323c188d2f7cb
jacando.tos <sereicikas.t@gmail.com>
2020-03-19 11:52:21 +0100

    render templates

--- index.js
+++ index.js
@@ -11,34 +11,54 @@ const server = http.createServer((req, res) => {
   const laptopId = url.parse(req.url, true).query.id;
 
   if (pathName === "/products" || pathName === "/") {
-
     res.writeHead(200, { "Content-type": "text/html" });
-    res.end("This is the Products page.");
-
+    fs.readFile(
+      `${__dirname}/templates/template-overview.html`,
+      "utf-8",
+      (err, data) => {
+        let overviewOutput = data;
+        
+        fs.readFile(
+          `${__dirname}/templates/template-card.html`,
+          "utf-8",
+          (err, data) => {
+            const cardsOutput = laptopData.map(el => replaceTemplate(data, el)).join("");
+            overviewOutput = overviewOutput.replace("{%CARDS%}", cardsOutput);
+            res.end(overviewOutput);
+          }
+        );
+      }
+    );
   } else if (pathName === "/laptop" && laptopId < laptopData.length) {
-
     res.writeHead(200, { "Content-type": "text/html" });
-    fs.readFile(`${__dirname}/templates/template-laptop.html`, "utf-8", (err, data) => {
-      const laptop = laptopData[laptopId];
-      let output = data.replace(/{%PRODUCTNAME%}/g, laptop.productName);
-      output = output.replace(/{%IMAGE%}/g, laptop.image);
-      output = output.replace(/{%PRICE%}/g, laptop.price);
-      output = output.replace(/{%SCREEN%}/g, laptop.screen);
-      output = output.replace(/{%CPU%}/g, laptop.cpu);
-      output = output.replace(/{%STORAGE%}/g, laptop.storage);
-      output = output.replace(/{%RAM%}/g, laptop.ram);
-      output = output.replace(/{%DESCRIPTION%}/g, laptop.description);
-      res.end(output);
-    });
-
+    fs.readFile(
+      `${__dirname}/templates/template-laptop.html`,
+      "utf-8",
+      (err, data) => {
+        const laptop = laptopData[laptopId];
+        const output = replaceTemplate(data, laptop);
+        res.end(output);
+      }
+    );
   } else {
-
     res.writeHead(404, { "Content-type": "text/html" });
     res.end("This is the 404 page.");
-
   }
 });
 
 server.listen(1337, "127.0.0.1", () => {
   console.log("Listening for requests now.");
 });
+
+function replaceTemplate(originalHtml, laptop) {
+  let output = originalHtml.replace(/{%PRODUCTNAME%}/g, laptop.productName);
+  output = output.replace(/{%IMAGE%}/g, laptop.image);
+  output = output.replace(/{%PRICE%}/g, laptop.price);
+  output = output.replace(/{%SCREEN%}/g, laptop.screen);
+  output = output.replace(/{%CPU%}/g, laptop.cpu);
+  output = output.replace(/{%STORAGE%}/g, laptop.storage);
+  output = output.replace(/{%RAM%}/g, laptop.ram);
+  output = output.replace(/{%DESCRIPTION%}/g, laptop.description);
+  output = output.replace(/{%ID%}/g, laptop.id);
+  return output;
+}
